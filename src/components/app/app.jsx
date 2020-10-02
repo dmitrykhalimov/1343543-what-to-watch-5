5c34494916565d918973ff6587b0c45a958c78c1
Bluesman <dmitry.khalimov@gmail.com>
2020-10-02 09:50:53 +0300

    Добавляет часть маршрутов, почему-то не работает film/:year

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
 import PageMain from "../page-main/page-main";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
+import SignIn from "../sign-in/sign-in";
+import MyList from "../my-list/my-list";
+import Film from "../film/film";
 
 const App = (props) => {
   const {title, genre, year} = props;
   return (
-    <PageMain
-      title = {title}
-      genre = {genre}
-      year = {year}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <PageMain
+            title = {title}
+            genre = {genre}
+            year = {year}
+          />
+        </Route>
+        <Route exact path="/login">
+          <SignIn />
+        </Route>
+        <Route exact path="/mylist">
+          <MyList />
+        </Route>
+        <Route exact path="/film/:year?" component={Film} />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,9 @@ module.exports = {
     },
     devServer: {
         contentBase: path.resolve(__dirname, 'public'),
-        open: false,
+        open: true,
         port: 3000,
+        historyApiFallback: true
     },
     module: {
         rules: [
