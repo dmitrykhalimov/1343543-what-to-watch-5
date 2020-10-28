a23825d020cac6565a45242d5f3a3428f292908c
Bluesman <dmitry.khalimov@gmail.com>
2020-10-28 19:06:26 +0300

    Превращает страницу плеер и плеер в функциональные компоненты

--- src/components/player/player.jsx
+++ src/components/player/player.jsx
@@ -3,22 +3,15 @@ import {connect} from "react-redux";
 import PropTypes from "prop-types";
 import VideoPlayerBig from "../video-player-big/video-player-big";
 import withActivePlayer from "../../hocs/with-active-tab/with-active-player";
-import VideoPlayer from "../video-player/video-player";
 
 const VideoPlayerBigWrapped = withActivePlayer(VideoPlayerBig);
-class Player extends PureComponent {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="player">
-        <VideoPlayerBigWrapped></VideoPlayerBigWrapped>
-      </div>
-    );
-  }
-}
+const Player = (props) => {
+  return (
+    <div className="player">
+      <VideoPlayerBigWrapped></VideoPlayerBigWrapped>
+    </div>
+  );
+};
 
 Player.propTypes = {};
 
--- src/components/video-player-big/video-player-big.jsx
+++ src/components/video-player-big/video-player-big.jsx
@@ -1,66 +1,58 @@
-import React, {PureComponent} from "react";
+import React from "react";
 import {connect} from "react-redux";
 import PropTypes from "prop-types";
 
-class VideoPlayerBig extends PureComponent {
-  constructor(props) {
-    super(props);
+const VideoPlayerBig = (props) => {
+  const {isPlaying, videoRef, progressRef, pinProgressRef, onPlayPauseClick, onFullscreenClick} = props;
+  return (
+    <React.Fragment>
+      <video
+        src="https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm"
+        className="player__video"
+        poster="/img/player-poster.jpg"
+        muted={true}
+        ref={videoRef}>
+      </video>
 
-  }
+      <button type="button" className="player__exit">Exit</button>
 
-  render() {
-    const {isPlaying, videoRef, progressRef, pinProgressRef, onPlayPauseClick, onFullscreenClick} = this.props;
-
-    return (
-      <React.Fragment>
-        <video
-          src="https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm"
-          className="player__video"
-          poster="/img/player-poster.jpg"
-          muted={true}
-          ref={videoRef}>
-        </video>
-
-        <button type="button" className="player__exit">Exit</button>
-
-        <div className="player__controls">
-          <div className="player__controls-row">
-            <div className="player__time">
-              <progress className="player__progress" value="30" max="100" ref={progressRef}></progress>
-              <div className="player__toggler" style={{left: `30%`}} ref={pinProgressRef}>Toggler</div>
-            </div>
-            <div className="player__time-value">1:30:29</div>
+      <div className="player__controls">
+        <div className="player__controls-row">
+          <div className="player__time">
+            <progress className="player__progress" value="30" max="100" ref={progressRef}></progress>
+            <div className="player__toggler" style={{left: `30%`}} ref={pinProgressRef}>Toggler</div>
           </div>
-
-          <div className="player__controls-row">
-            <button
-              type="button"
-              className="player__play"
-              onClick={onPlayPauseClick}
-            >
-              <svg viewBox="0 0 19 19" width="19" height="19">
-                <use xlinkHref={isPlaying ? `#pause` : `#play-s`}></use>
-              </svg>
-              <span>Play</span>
-            </button>
-            <div className="player__name">Transpotting</div>
-
-            <button
-              type="button"
-              className="player__full-screen"
-              onClick={onFullscreenClick}
-            >
-              <svg viewBox="0 0 27 27" width="27" height="27">
-                <use xlinkHref="#full-screen"></use>
-              </svg>
-              <span>Full screen</span>
-            </button>
-          </div>
+          <div className="player__time-value">1:30:29</div>
         </div>
-      </React.Fragment>
-    );
-  }
-}
+
+        <div className="player__controls-row">
+          <button
+            type="button"
+            className="player__play"
+            onClick={onPlayPauseClick}
+          >
+            <svg viewBox="0 0 19 19" width="19" height="19">
+              <use xlinkHref={isPlaying ? `#pause` : `#play-s`}></use>
+            </svg>
+            <span>Play</span>
+          </button>
+          <div className="player__name">Transpotting</div>
+
+          <button
+            type="button"
+            className="player__full-screen"
+            onClick={onFullscreenClick}
+          >
+            <svg viewBox="0 0 27 27" width="27" height="27">
+              <use xlinkHref="#full-screen"></use>
+            </svg>
+            <span>Full screen</span>
+          </button>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
 
 VideoPlayerBig.propTypes = {
   isPlaying: PropTypes.bool.isRequired,
