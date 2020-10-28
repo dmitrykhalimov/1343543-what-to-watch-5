038234dc1c4d26599d73e3d1d08de560a922b493
Bluesman <dmitry.khalimov@gmail.com>
2020-10-28 21:01:15 +0300

    Все прекрасно работает. Теперь точно

--- src/components/more-like-this/more-like-this.jsx
+++ src/components/more-like-this/more-like-this.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const MoreLikeThis = (props) => {
-  console.log(props.children);
   return (
     <section className="catalog catalog--like-this">
       <h2 className="catalog__title">More like this</h2>
