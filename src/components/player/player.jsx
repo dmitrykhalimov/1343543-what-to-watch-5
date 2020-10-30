ed7fcbc7d1448d612866959a014a182572c1bdf9
Bluesman <dmitry.khalimov@gmail.com>
2020-10-30 16:43:33 +0300

    Исправляет баг с расположением хока плеера

--- src/components/player/player.jsx
+++ src/components/player/player.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {connect} from "react-redux";
 import PropTypes from "prop-types";
 import VideoPlayerBig from "../video-player-big/video-player-big";
-import withActivePlayer from "../../hocs/with-active-tab/with-active-player";
+import withActivePlayer from "../../hocs/with-active-player/with-active-player";
 import {validFilm} from "../../utils/props";
 import {useParams} from "react-router-dom";
 
