ad8c4ae1e65ceb242bced6b9b3db0d06cb14ecda
Bluesman <dmitry.khalimov@gmail.com>
2020-11-03 13:08:16 +0300

    Переводит часть разметки в компоненты

---
+++ src/components/button-play/button-play.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ButtonPlay = () => {
+
+  return (
+    <React.Fragment>
+      <svg viewBox="0 0 19 19" width="19" height="19">
+        <use xlinkHref="#play-s"></use>
+      </svg>
+      <span>Play</span>
+    </React.Fragment>
+  );
+};
+
+export default ButtonPlay;
--- src/components/page-main/page-main.jsx
+++ src/components/page-main/page-main.jsx
@@ -8,13 +8,15 @@ import Filter from "../filter/filter";
 import Footer from "../footer/footer";
 
 import {changeGenre, incrementRendered, resetRendered} from "../../store/action";
-import {validFilm} from "../../utils/props";
+import {validFilm, validUserData} from "../../utils/props";
 import ShowMore from "../show-more/show-more";
 import FilmsCatalog from "../films-catalog/films-catalog";
 import PageContent from "../page-content/page-content";
 import {Link} from "react-router-dom";
 import {getRendered, getGenresList, getActiveGenre, getFilms, getFilteredFilms, getUserData} from "../../store/reducers/selectors";
 import UserBlock from "../user-block/user-block";
+import Logo from "../logo/logo";
+import ButtonPlay from "../button-play/button-play";
 
 
 const PageMain = (props) => {
@@ -42,14 +44,7 @@ const PageMain = (props) => {
         <h1 className="visually-hidden">WTW</h1>
 
         <header className="page-header movie-card__head">
-          <div className="logo">
-            <a className="logo__link">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
-
+          <Logo/>
           <UserBlock
             userData = {userData}
           />
@@ -71,10 +66,7 @@ const PageMain = (props) => {
               <div className="movie-card__buttons">
                 {/* TODO: Перерисовывается кнопка воспроизвести фильм, придумать чего делать с этим*/}
                 <Link className="btn btn--play movie-card__button" type="button" to={`/films/1`}>
-                  <svg viewBox="0 0 19 19" width="19" height="19">
-                    <use xlinkHref="#play-s"></use>
-                  </svg>
-                  <span>Play</span>
+                  <ButtonPlay/>
                 </Link>
                 <button className="btn btn--list movie-card__button" type="button">
                   <svg viewBox="0 0 19 20" width="19" height="20">
@@ -124,6 +116,7 @@ PageMain.propTypes = {
   genresList: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
   rendered: PropTypes.number.isRequired,
   incrementRenderedFilms: PropTypes.func.isRequired,
+  userData: validUserData,
 };
 
 const mapStateToProps = (state) => ({
