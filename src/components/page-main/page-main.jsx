6a3e7e4827676f72a72d3796200ee914a6184b2a
Bluesman <dmitry.khalimov@gmail.com>
2020-11-03 11:06:04 +0300

    Промежуточная фиксация

--- src/components/page-main/page-main.jsx
+++ src/components/page-main/page-main.jsx
@@ -69,6 +69,7 @@ const PageMain = (props) => {
               </p>
 
               <div className="movie-card__buttons">
+                {/* TODO: Перерисовывается кнопка воспроизвести фильм, придумать чего делать с этим*/}
                 <Link className="btn btn--play movie-card__button" type="button" to={`/films/1`}>
                   <svg viewBox="0 0 19 19" width="19" height="19">
                     <use xlinkHref="#play-s"></use>
