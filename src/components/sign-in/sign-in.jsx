f2e8d097e57214fccbdc01f4fbed03bbad353d6d
Bluesman <dmitry.khalimov@gmail.com>
2020-11-03 15:12:51 +0300

    Организовывает функцию-хэндлер

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -1,36 +1,48 @@
-import React, {PureComponent} from "react";
+import React, {PureComponent, createRef} from "react";
+import Logo from "../logo/logo";
 
 // чтобы получить 100 баллов, нужно классовые компоненты заменить на хуки, но и так голова кипит - отрефакторю потом.
 
 class SignIn extends PureComponent {
   constructor(props) {
     super(props);
+
+    this.emailRef = createRef();
+    this.passwordRef = createRef();
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+
+    console.log(this.emailRef.current.value);
+    console.log(this.passwordRef.current.value);
+
+
   }
 
   render() {
     return (
       <div className="user-page">
         <header className="page-header user-page__head">
-          <div className="logo">
-            <a href="main.html" className="logo__link">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
-
+          <Logo/>
           <h1 className="page-title user-page__title">Sign in</h1>
         </header>
 
         <div className="sign-in user-page__content">
-          <form action="#" className="sign-in__form">
+          <form
+            action="#"
+            className="sign-in__form"
+            onSubmit={this.handleSubmit}
+          >
             <div className="sign-in__fields">
               <div className="sign-in__field">
-                <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email" />
+                <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email" ref={this.emailRef}/>
                 <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
               </div>
               <div className="sign-in__field">
-                <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" />
+                <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" ref={this.passwordRef}/>
                 <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
               </div>
             </div>
@@ -42,6 +54,7 @@ class SignIn extends PureComponent {
 
         <footer className="page-footer">
           <div className="logo">
+            {/* todo - управляемое лого */}
             <a href="main.html" className="logo__link logo__link--light">
               <span className="logo__letter logo__letter--1">W</span>
               <span className="logo__letter logo__letter--2">T</span>
