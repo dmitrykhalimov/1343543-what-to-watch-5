0e8d492af9f557b8b56f90c4df32f0c95b374980
Bluesman <dmitry.khalimov@gmail.com>
2020-11-05 20:00:58 +0300

    fix #4 Исправляет стилизацию страницы AddReview

--- src/components/add-review/add-review.jsx
+++ src/components/add-review/add-review.jsx
@@ -1,15 +1,32 @@
-import React from "react";
-// import PropTypes from "prop-types";
+import React, { useEffect } from "react";
+
+import PropTypes from "prop-types";
+import {getActiveFilm} from "../../store/reducers/selectors";
+import {withRouter} from "react-router";
+import {connect} from "react-redux";
+
 import FormReview from "../form-review/form-review";
 import Logo from "../logo/logo";
 import UserBlock from "../user-block/user-block";
+import {fetchSingleFilm} from "../../store/api-actions";
 
-const AddReview = () => {
+const AddReview = (props) => {
+  const id = props.match.params.id;
+  const {handlePageLoad, activeFilm} = props;
+
+  const backgroundStyle = {
+    backgroundColor: activeFilm.backgroundColor,
+  };
+
+  useEffect(() => {
+    handlePageLoad(id);
+  }, []);
+
   return (
-    <section className="movie-card movie-card--full">
+    <section className="movie-card movie-card--full" style={backgroundStyle}>
       <div className="movie-card__header">
         <div className="movie-card__bg">
-          <img src="/img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" />
+          <img src={activeFilm.background} alt={activeFilm.title} />
         </div>
 
         <h1 className="visually-hidden">WTW</h1>
@@ -19,7 +36,7 @@ const AddReview = () => {
           <nav className="breadcrumbs">
             <ul className="breadcrumbs__list">
               <li className="breadcrumbs__item">
-                <a href="movie-page.html" className="breadcrumbs__link">The Grand Budapest Hotel</a>
+                <a href="movie-page.html" className="breadcrumbs__link">{activeFilm.title}</a>
               </li>
               <li className="breadcrumbs__item">
                 <a className="breadcrumbs__link">Add review</a>
@@ -31,7 +48,7 @@ const AddReview = () => {
         </header>
 
         <div className="movie-card__poster movie-card__poster--small">
-          <img src="/img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218" height="327" />
+          <img src={activeFilm.poster} alt={`${activeFilm.title} poster`} width="218" height="327" />
         </div>
       </div>
 
@@ -43,4 +60,16 @@ const AddReview = () => {
 
 AddReview.propTypes = {};
 
-export default AddReview;
+const mapStateToProps = (state) => ({
+  activeFilm: getActiveFilm(state),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  handlePageLoad(id) {
+    dispatch(fetchSingleFilm(id));
+  },
+});
+
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(AddReview));
+
