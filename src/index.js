5abb6e758008f36fa6bd3914caa5ffec5fd68d89
Bluesman <dmitry.khalimov@gmail.com>
2020-11-05 11:32:56 +0300

    Промежуточная фиксация

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./components/app/app";
 import ErrorPage from "./components/error-page/error-page";
 import rootReducer from "./store/reducers/root-reducer";
 import {requireAuthorization} from "./store/action";
-import {fetchFilmsList, checkAuth, fetchFilmPromo, fetchFavorites} from "./store/api-actions";
+import {fetchFilmsList, checkAuth, fetchFilmPromo} from "./store/api-actions";
 import thunk from "redux-thunk";
 import {createAPI} from "./services/api";
 import {AuthorizationStatus} from "./const";
@@ -48,12 +48,9 @@ Promise.all([
 // TODO:
 // Почему перерисовывается 4 раза?
 
-// checkAuth очень странно работает
-// не отображается avatar.
-
 // переписать на HOC-компонент formReview
 // блокировка экрана при отправке комментариев
 // Refactor: propslibrary
 // * подумать, нет ли возможности заменить history на Redirect в film.
 // * сделать разные коды ошибки
-// перенести фетч фильмов в mylist
+
