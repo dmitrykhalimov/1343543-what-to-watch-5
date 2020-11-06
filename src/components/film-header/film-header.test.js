190ee99eeabe536d5050e9f7d764effb6e54ac05
Bluesman <dmitry.khalimov@gmail.com>
2020-11-06 11:06:09 +0300

    Добавляет testmockstore

---
+++ src/components/film-header/film-header.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import FilmHeader from "./film-header";
+import {TEST_MOCKS, TEST_MOCK_STORE} from "../../const";
+import {MemoryRouter} from 'react-router-dom';
+import configureMockStore from "redux-mock-store";
+import {Provider} from "react-redux";
+
+const mockStore = configureMockStore();
+const store = mockStore(TEST_MOCK_STORE);
+
+describe(`FilmHeader`, () => {
+  it(`Should FilmHeader render correctly`, () => {
+    const tree = renderer
+      .create(
+          <Provider store={store}>
+            <MemoryRouter>
+              <FilmHeader
+                title={TEST_MOCKS.title}
+                background={TEST_MOCKS.background}
+              />
+            </MemoryRouter>
+          </Provider>
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
+
