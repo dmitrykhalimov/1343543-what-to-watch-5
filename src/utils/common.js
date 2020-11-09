fdd51fac656fa0602f6b8d6f1ccea5402c3fa89d
Bluesman <dmitry.khalimov@gmail.com>
2020-11-10 01:29:29 +0300

    Б10-Б37

--- src/utils/common.js
+++ src/utils/common.js
@@ -9,14 +9,6 @@ const LocaleSetting = {
   REGION: `en-US`,
 };
 
-
-export const getRandomInteger = (min = 0, max = 1) => {
-  const lower = Math.ceil(Math.min(min, max));
-  const upper = Math.floor(Math.max(min, max));
-
-  return Math.floor(lower + Math.random() * (upper - lower + 1));
-};
-
 export const translateRatingToText = (rating) => {
   const result = ratingRanks
     .find((item) => {
